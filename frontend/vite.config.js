e445fab00eb01ccc0bc3e131a8ea14079e6abbf9
muhammadkhuzaima25 <muhammadkhuzaima.gcu@gmail.com>
2026-05-31 22:20:05 +0500

    Updated with vercel deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://resumatchai-three.vercel.app',
+        target: 'http://localhost:5000',
         changeOrigin: true,
       },
     },
